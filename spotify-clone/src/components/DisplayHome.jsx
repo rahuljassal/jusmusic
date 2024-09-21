21d56cbf39508e788d7f7d5a785e0148289603a6
rahul <53050874+rahuljassal@users.noreply.github.com>
2024-09-21 20:45:57 +0530

    REF : song tile size

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
 // import { albumsData } from '../assets/assets'
-import AlbumItem from './AlbumItem'
+import AlbumItem from "./AlbumItem";
 // import { songsData } from '../assets/assets'
-import SongItem from './SongItem'
-import { useContext } from 'react'
-import { PlayerContext } from '../context/PlayerContext'
+import SongItem from "./SongItem";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayHome = () => {
-
-  const { songsData,albumsData } = useContext(PlayerContext);
+  const { songsData, albumsData } = useContext(PlayerContext);
 
   return (
     <>
-     <Navbar />
-     <div className='mb-4'>
-      <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
-      <div className='flex overflow-auto'>
-          {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
+      <Navbar />
+      <div className="mb-4">
+        {albumsData.length ? (
+          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        ) : null}
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
-     </div>
-     <div className='mb-4'>
-      <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
-      <div className='flex overflow-auto'>
-          {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image} />))}
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item._id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
-     </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
