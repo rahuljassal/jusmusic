b1e03ab5e141aec662a0f2b17bc9bf8bfd2baea3
rahul <53050874+rahuljassal@users.noreply.github.com>
2024-09-23 01:41:32 +0530

    REF: updated tile size

--- spotify-clone/src/components/AlbumItem.jsx
+++ spotify-clone/src/components/AlbumItem.jsx
@@ -7,7 +7,7 @@ const AlbumItem = ({ image, name, desc, id }) => {
   return (
     <div
       onClick={() => navigate(`/album/${id}`)}
-      className="min-w-[180px] max-w-[200px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
+      className="min-w-[180px] max-w-[200px] aspect-square p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
     >
       <img className="rounded" src={image} alt="" />
       <p className="font-bold mt-2 mb-1">{name}</p>
--- spotify-clone/src/components/SongItem.jsx
+++ spotify-clone/src/components/SongItem.jsx
@@ -7,7 +7,7 @@ const SongItem = ({ name, image, desc, id }) => {
   return (
     <div
       onClick={() => playWithId(id)}
-      className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26] max-w-[200px]"
+      className="min-w-[180px] p-2 px-3 aspect-square rounded cursor-pointer hover:bg-[#ffffff26] max-w-[200px]"
     >
       <img className="rounded" src={image} alt="" />
       <p className="font-bold mt-2 mb-1">{name}</p>
