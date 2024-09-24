cee3a324781278b93b03ec50535341ba15f70250
rahul <53050874+rahuljassal@users.noreply.github.com>
2024-09-24 15:53:55 +0530

    REF : updated home page

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -13,13 +13,12 @@ const DisplayHome = () => {
   return (
     <>
       <Navbar />
+
       <div className="mb-4">
-        {albumsData.length ? (
-          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
-        ) : null}
-        <div className="grid grid-flow-row-dense lg:grid-cols-4 md:grid-cols-4 overflow-auto grid-cols-2">
-          {albumsData.map((item, index) => (
-            <AlbumItem
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="grid grid-flow-row  overflow-auto lg:grid-cols-4 md:grid-cols-4 grid-cols-2">
+          {songsData.map((item, index) => (
+            <SongItem
               key={index}
               name={item.name}
               desc={item.desc}
@@ -30,10 +29,12 @@ const DisplayHome = () => {
         </div>
       </div>
       <div className="mb-4">
-        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
-        <div className="grid grid-flow-row  overflow-auto lg:grid-cols-4 md:grid-cols-4 grid-cols-2">
-          {songsData.map((item, index) => (
-            <SongItem
+        {albumsData.length ? (
+          <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        ) : null}
+        <div className="grid grid-flow-row-dense lg:grid-cols-4 md:grid-cols-4 overflow-auto grid-cols-2">
+          {albumsData.map((item, index) => (
+            <AlbumItem
               key={index}
               name={item.name}
               desc={item.desc}
